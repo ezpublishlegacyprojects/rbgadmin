09623448d2e68ecd266a56aee07f008b007337d0
Gaetano Giunta <gg@ez.no>
2009-06-26 09:02:53 +0000

    - added some informative files
    git-svn-id: http://svn.projects.ez.no/rbgadmin@2 62a8d394-6a09-4319-997a-d2f7c5838de3

---
+++ trunk/extension/rbgadmin/ezinfo.php
@@ -0,0 +1,13 @@
+<?php
+class ggsysinfoInfo
+{
+    static function info()
+    {
+        return array( 'Name' => "rbgadmin",
+                      'Version' => "0.1",
+                      'Copyright' => "Copyright (C) 2009 R. Benedetti",
+                      'License' => "GNU General Public License v2.0"
+                     );
+    }
+}
+?>
